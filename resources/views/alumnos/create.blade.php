d4ce9098e0d94ed9e79ae93e8e8fd227174b5023
martinezjone <122877061+martinezjone@users.noreply.github.com>
2023-12-21 12:15:20 +0100

    main branch

--- resources/views/alumnos/create.blade.php
+++ resources/views/alumnos/create.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
     <h1>IKASLE BERRIA</h1>
+    {{-- Konponeteekin eginda --}}
 
     <form action="{{ route('alumnos.store')}}" method="post" enctype="multipart/form-data" class="mt-6 space-y-6">
         @csrf
